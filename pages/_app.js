03557097f07ed733f90434f00599851bb2d1cfca
EmojiCat <emojiotaku@gmail.com>
2022-10-03 20:20:42 +0900

    Add Google AdSense for the MONEY

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 // pages/_app.js
-import Home from './index.js';
-import './css/card.css'
+import Script from 'next/script';
+import './css/card.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Home {...pageProps} />
+  return (
+    <>
+      <Script
+        id="Adsense-id" data-ad-client="ca-pub-4352778397803727"
+        async strategy="afterInteractive"
+        onError={ (e) => { console.error('Script failed to load', e) }}
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
+      <Component {...pageProps} />
+    </>
+  )
 };

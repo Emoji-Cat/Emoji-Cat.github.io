7abfa3cfb71a6542d2f3a7158b85f23c3fccbedd
EmojiCat <emojiotaku@gmail.com>
2022-10-08 18:10:43 +0900

    Fix some Script for Adsense

--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,11 @@ export default function Document() {
     <Html>
       <Head>
         <Script
-            id="Adsense-id" data-ad-client="ca-pub-4352778397803727"
+            id="Adsense-id"
             async strategy="afterInteractive"
             onError={ (e) => { console.error('Script failed to load', e) }}
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4352778397803727"
+            crossorigin="anonymous"
         />
       </Head>
       <body>

f9df74776599d75eeba659223b123097d5e30ea2
EmojiCat <emojiotaku@gmail.com>
2022-10-14 19:34:37 +0900

    Fix to run Adsense

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // pages/_app.js
 import Head from 'next/head'
+import Script from 'next/script'
 import './css/card.css'
 import './css/grid.css'
 
@@ -11,6 +12,13 @@ export default function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+      <Script
+        id="Adsense-id"
+        async strategy="beforeInteractive"
+        onError={ (e) => { console.error('Script failed to load', e) }}
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4352778397803727"
+        crossOrigin="anonymous"
+      />
       <Component {...pageProps} />
     </>
   )
--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <Script
-            id="Adsense-id"
-            async strategy="afterInteractive"
-            onError={ (e) => { console.error('Script failed to load', e) }}
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4352778397803727"
-            crossorigin="anonymous"
-        />
       </Head>
       <body>
         <Main />
